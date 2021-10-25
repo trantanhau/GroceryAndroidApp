c406092c7f0399072a2b85615877549f641fca69
trantanhau <trantanhau281015@gmail.com>
2021-10-26 01:43:40 +0700

    add swap intent between signIn and signUp page

--- app/src/main/java/com/example/groceryandroidapp/LoginActivity.java
+++ app/src/main/java/com/example/groceryandroidapp/LoginActivity.java
@@ -2,13 +2,39 @@ package com.example.groceryandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button signIn;
+    TextView email, password;
+    TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signIn = findViewById(R.id.login_button);
+        signUp =  findViewById(R.id.sign_up);
+        email = findViewById(R.id.email_register);
+        password = findViewById(R.id.password_register);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/groceryandroidapp/RegisterActivity.java
+++ app/src/main/java/com/example/groceryandroidapp/RegisterActivity.java
@@ -2,13 +2,42 @@ package com.example.groceryandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button signUp;
+    TextView name, email, password;
+    TextView signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        signUp = findViewById(R.id.register_button);
+        signIn =  findViewById(R.id.sign_in);
+        name = findViewById(R.id.name_input);
+        email = findViewById(R.id.email_register);
+        password = findViewById(R.id.password_register);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }

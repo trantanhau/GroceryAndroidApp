a62b46423b2a29169484b64ea44a40176e94580b
trantanhau <trantanhau281015@gmail.com>
2021-10-26 13:42:10 +0700

    using firebase-store and firestore-db to manage data

---
+++ app/src/main/java/com/example/groceryandroidapp/adapters/PopularProductAdapters.java
@@ -0,0 +1,64 @@
+package com.example.groceryandroidapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.groceryandroidapp.R;
+import com.example.groceryandroidapp.models.PopularProductModel;
+
+import java.util.List;
+
+public class PopularProductAdapters extends RecyclerView.Adapter<PopularProductAdapters.ViewHolder> {
+
+    private Context context;
+
+    public PopularProductAdapters(Context context, List<PopularProductModel> popularProductModels) {
+        this.context = context;
+        this.popularProductModelsList = popularProductModels;
+    }
+
+    private List<PopularProductModel> popularProductModelsList;
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.popurlar_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Glide.with(context).load(popularProductModelsList.get(position).getImg_url()).into(holder.popular_product_image);
+        holder.name.setText(popularProductModelsList.get(position).getName());
+        holder.description.setText(popularProductModelsList.get(position).getDescription());
+        holder.rating.setText(popularProductModelsList.get(position).getRating());
+        holder.discount.setText(popularProductModelsList.get(position).getDiscount());
+    }
+
+    @Override
+    public int getItemCount() {
+        return popularProductModelsList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView popular_product_image;
+        TextView name, description,  rating, discount;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            popular_product_image = itemView.findViewById(R.id.popular_image);
+            name = itemView.findViewById(R.id.popular_product_name);
+            description = itemView.findViewById(R.id.popular_product_description);
+            rating = itemView.findViewById(R.id.popular_product_rating);
+            discount = itemView.findViewById(R.id.popular_product_discount);
+        }
+    }
+}
--- app/src/main/java/com/example/groceryandroidapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/groceryandroidapp/ui/home/HomeFragment.java
@@ -4,21 +4,60 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.groceryandroidapp.adapters.PopularProductAdapters;
 import com.example.groceryandroidapp.databinding.FragmentHomeBinding;
+import com.example.groceryandroidapp.models.PopularProductModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
-
+    RecyclerView popularProductRecycle;
+    List<PopularProductModel> popularProductModelList;
+    PopularProductAdapters popularProductAdapters;
+    FirebaseFirestore db;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        db = FirebaseFirestore.getInstance();
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        popularProductRecycle = binding.popularProduct;
+        popularProductRecycle.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        popularProductModelList = new ArrayList<>();
+        popularProductAdapters = new PopularProductAdapters(getActivity(), popularProductModelList);
+        popularProductRecycle.setAdapter(popularProductAdapters);
+        db.collection("PopularProduct")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                PopularProductModel popularProductModel = document.toObject(PopularProductModel.class);
+                                popularProductModelList.add(popularProductModel);
+                                popularProductAdapters.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error "+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
         return root;
     }
 

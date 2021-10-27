1fac8fcc783550be5252d0b55cdce3a4f0a67301
trantanhau <trantanhau281015@gmail.com>
2021-10-27 10:23:20 +0700

    add calculate total and price to firebase storage

--- app/src/main/java/com/example/groceryandroidapp/activities/DetailActivity.java
+++ app/src/main/java/com/example/groceryandroidapp/activities/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.groceryandroidapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,10 +8,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.groceryandroidapp.R;
 import com.example.groceryandroidapp.models.ProductModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class DetailActivity extends AppCompatActivity {
     TextView quantity;
@@ -21,12 +32,18 @@ public class DetailActivity extends AppCompatActivity {
     Button addToCartButton;
     ImageView addItem, removeItem;
 
+    FirebaseFirestore firestore;
+    FirebaseAuth auth;
+
     ProductModel productModel = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         final Object object = getIntent().getSerializableExtra("detail");
         if (object instanceof ProductModel){
@@ -67,6 +84,8 @@ public class DetailActivity extends AppCompatActivity {
                 if (totalQuantity < 10){
                     totalQuantity++;
                     quantity.setText(String.valueOf(totalQuantity));
+                    totalPrice = productModel.getPrice() * totalQuantity;
+
                 }
             }
         });
@@ -76,8 +95,45 @@ public class DetailActivity extends AppCompatActivity {
                 if (totalQuantity > 0){
                     totalQuantity--;
                     quantity.setText(String.valueOf(totalQuantity));
+                    totalPrice = productModel.getPrice() * totalQuantity;
+
                 }
             }
         });
+        addToCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addToCart();
+            }
+        });
+    }
+    private void addToCart() {
+        String saveCurrentDate, saveCurrentTime;
+        Calendar calForDate = Calendar.getInstance();
+
+        SimpleDateFormat currentDate =  new SimpleDateFormat("MM dd, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        SimpleDateFormat currentTime =  new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calForDate.getTime());
+
+        final HashMap<String, Object> cartMap = new HashMap<>();
+
+        cartMap.put("productName", productModel.getName());
+        cartMap.put("productPrice", price.getText().toString());
+        cartMap.put("currentDate", saveCurrentDate);
+        cartMap.put("currentTime", saveCurrentTime);
+        cartMap.put("totalQuantity", quantity.getText().toString());
+        cartMap.put("totalPrice", totalPrice);
+
+        firestore.collection("AddToCart").document(auth.getCurrentUser().getUid()).collection("CurrentUser").add(cartMap)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Toast.makeText(DetailActivity.this, "Added to Cart", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+
     }
 }
